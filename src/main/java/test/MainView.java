fbe286b5ef6443a108c562404789d3b1efbdc621
Arnaud Brunet <arnaud.brunet@unc.nc>
2019-11-26 08:01:33 +1100

    solution

--- src/main/java/test/MainView.java
+++ src/main/java/test/MainView.java
@@ -1,5 +1,6 @@
 package test;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -9,6 +10,9 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
     
     public MainView(DialogContentProvider contentProvider) {
-        this.add(new Button("Click me!", event -> new Dialog(contentProvider.create()).open()));
+        this.add(new Button("Click me!", event -> { 
+            Component content = contentProvider.create();
+            new Dialog(content).open();
+        }));
     }
 }
