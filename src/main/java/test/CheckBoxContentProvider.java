a0e5b5b5193e9d69ef8ae0969f8bbc2eb9859874
Arnaud Brunet <arnaud.brunet@unc.nc>
2020-08-03 15:10:13 +1100

    Update versions

--- src/main/java/test/CheckBoxContentProvider.java
+++ src/main/java/test/CheckBoxContentProvider.java
@@ -2,12 +2,11 @@ package test;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.router.Route;
 
 // Les composants dynamiques du provider ne sont pas chargés par Vaadin
 // Utilisation d'une fausse route pour forcer le chargement
 // voir https://vaadin.com/forum/thread/17765421/14-npm-mode-com-vaadin-flow-component-internal-componentmetadata-front
-@Route(registerAtStartup = false)
+//@Route(registerAtStartup = false)
 public class CheckBoxContentProvider implements DialogContentProvider {
     @Override
     public Component create() {
